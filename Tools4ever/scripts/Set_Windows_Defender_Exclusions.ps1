cd61304e61e87011792e51afef1f5c34088e7999
Mike Sheldon <m.sheldon@tools4ever.com>
2023-04-20 10:09:16 -0800

    Change folder structure for scripts, add proxy and exclusions

---
+++ Tools4ever/scripts/Set_Windows_Defender_Exclusions.ps1
@@ -0,0 +1,4 @@
+Add-MpPreference -ExclusionPath "C:\Program Files\Tools4ever\NIM"
+Add-MpPreference -ExclusionPath "C:\Program Files\Tools4ever\NIM\nim.exe"
+Add-MpPreference -ExclusionPath "C:\Program Files\Tools4ever\NIM\node.exe"
+Add-MpPreference -ExclusionPath "C:\ProgramData\Tools4ever\NIM"
