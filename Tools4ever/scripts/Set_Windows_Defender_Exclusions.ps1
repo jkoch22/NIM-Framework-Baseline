fba2ac6b219896d7bf650342c0406a64a437b7d1
Mike Sheldon <m.sheldon@tools4ever.com>
2023-06-22 09:53:09 -0800

    Update Set_Windows_Defender_Exclusions.ps1
    Pull NIM Installation path from Registry for windows Defender setup

--- Tools4ever/scripts/Set_Windows_Defender_Exclusions.ps1
+++ Tools4ever/scripts/Set_Windows_Defender_Exclusions.ps1
@@ -1,4 +1,7 @@
-Add-MpPreference -ExclusionPath "C:\Program Files\Tools4ever\NIM"
-Add-MpPreference -ExclusionPath "C:\Program Files\Tools4ever\NIM\nim.exe"
-Add-MpPreference -ExclusionPath "C:\Program Files\Tools4ever\NIM\node.exe"
-Add-MpPreference -ExclusionPath "C:\ProgramData\Tools4ever\NIM"
+$NimService = (Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\NIM -Name ImagePath).ImagePath -replace '"'
+$NimData = (Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\NIM\Config -Name workingPath).workingPath -replace '"'
+
+Add-MpPreference -ExclusionPath "$((Get-Item -Path $NimService).DirectoryName)"
+Add-MpPreference -ExclusionPath "$($NimService)"
+Add-MpPreference -ExclusionPath "$((Get-Item -Path $NimService).DirectoryName)\node.exe"
+Add-MpPreference -ExclusionPath "$($NimData)"
