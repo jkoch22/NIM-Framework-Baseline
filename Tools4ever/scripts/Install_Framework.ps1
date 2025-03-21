5111e2bf5f9512ca9b3f959ea0a79b0488d19a50
Mike Sheldon <m.sheldon@tools4ever.com>
2025-03-21 10:55:37 -0800

    Update Install_Framework.ps1
    - Check if running as Admin
    - Update input variable name
    - Add HTTP/HTTPS Settings
    - Install and Set Default app
    - Add option to restart service

--- Tools4ever/scripts/Install_Framework.ps1
+++ Tools4ever/scripts/Install_Framework.ps1
@@ -1,4 +1,11 @@
 Write-Output "Starting Framework installation"
+
+# Check if the script is running as an administrator
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Error "This script must be run as an administrator. Please restart the script as an administrator."
+    break
+}
+
 # Prompt for path input
 	$path = Read-Host -Prompt "Path of Tools4ever Directory [Default: C:\Tools4ever]"
 	Write-Output ""
@@ -52,6 +59,10 @@ Write-Output "Starting Framework installation"
 # Clean Up Installation files
 	Remove-Item $tempPath -Recurse -Force -Confirm:$false
 
+# Copy Dashboard App
+    Write-Output "Installing Dashboard App..."
+    Copy-Item -Recurse -Path "$($path)\_source\ProgramData\Tools4ever\NIM\config\apps\Dashboard" -Destination "$($installationPath)\apps" -Force
+
 # Copy Schema files
 	Write-Output "Installing schema files..."
 	Copy-Item -Recurse -Path "$($path)\_source\ProgramData\Tools4ever\NIM\config\rest\" -Destination "$($installationPath)" -Force
@@ -62,22 +73,53 @@ Write-Output "Starting Framework installation"
 
 # Configure Memory Settings
     Write-Output ""
-    $input = Read-Host -Prompt "Configure Memory Settings for Service? [Default: Y]"
-    if($input -ne 'N') {
+    $promptInput = Read-Host -Prompt "Configure Memory Settings for Service? [Default: Y]"
+    if($promptInput -ne 'N') {
         Write-Output "Configuring Memory Settings for Service..."
         Invoke-Command {reg import "$($path)\scripts\Set_NIM_Memory_16gb.reg" *>&1 | Out-Null}    
     }
 
 # Update Service Settings
-Write-Output ""
-    $input = Read-Host -Prompt "Update Service Settings? [Default: Y]"
-    if($input -ne 'N') {
+    Write-Output ""
+    $promptInput = Read-Host -Prompt "Update Service Settings? [Default: Y]"
+    if($promptInput -ne 'N') {
         Write-Output "Configuring Service Settings ..."
         $jsonFilePath = "$($installationPath)\settings.json"
+	
+ 	
+        # Set default ports
+        $http_port = 80	
+        $https_port = 443
         
+        $promptInput = Read-Host -Prompt "HTTP Port? [Default: 80]"
+        if($promptInput.length -gt 0) {
+            if ([double]::TryParse($promptInput, [ref]$promptInput)) {
+                $http_port = $promptInput
+            }
+        }
+        
+        $promptInput = Read-Host -Prompt "HTTPS Port? [Default: 443]"
+        if($promptInput.length -gt 0) {
+            if ([double]::TryParse($promptInput, [ref]$promptInput)) {
+                $https_port = $promptInput
+            }
+        }
+ 	
+ 
         # Check if the JSON file exists, if not, create it with default content
         if (-Not (Test-Path -Path $jsonFilePath)) {
             $defaultContent = @{
+                http = @{
+                    enabled = $true
+                    port = $http_port
+                }
+                https = @{
+                    enabled = $true
+                    port = $https_port
+                    cert_name = "self_signed"
+                }
+        	    default_app = "Dashboard"
+	 	        default_preview_count = 10000
                 mfa = "Optional"
                 worker_thread_auditing_query_max = 10
                 worker_thread_auditing_storage = $true
@@ -86,6 +128,35 @@ Write-Output ""
         } else {
             # Read the JSON file
             $jsonContent = Get-Content -Path $jsonFilePath -Raw | ConvertFrom-Json
+
+            # Update the "http" property to "enabled" and "port" to the specified values
+            if($null -eq $jsonContent.http) {
+                $jsonContent | Add-Member -MemberType NoteProperty -Name "http" -Value @{
+                    enabled = $true
+                    port = $http_port
+                }
+            }
+            $jsonContent.http.enabled = $true
+            $jsonContent.http.port = $http_port
+
+            # Update the "https" property to "enabled", "port" and "cert_name" to the specified values
+            if($null -eq $jsonContent.https) {
+                $jsonContent | Add-Member -MemberType NoteProperty -Name "https" -Value @{
+                    enabled = $true
+                    port = $https_port
+                    cert_name = "self_signed"
+                }
+            }
+            $jsonContent.https.enabled = $true
+            $jsonContent.https.port = $https_port
+            $jsonContent.https.cert_name = "self_signed"
+
+            # Update the "default_app" property to "Dashboard", if it doesn't exist, add it
+            if ($null -eq $jsonContent.default_app) {
+                $jsonContent | Add-Member -MemberType NoteProperty -Name "default_app" -Value "Dashboard"
+            } else {
+                $jsonContent.default_app = "Dashboard"
+            }
 
             # Update the "mfa" property to "Optional", if it doesn't exist, add it
             if ($null -eq $jsonContent.mfa) {
@@ -115,23 +186,22 @@ Write-Output ""
 
 
 # Setup Windows Defender
-    $input = Read-Host -Prompt "Configure Windows Defender Exclusions? [Default: Y]"
-    if($input -ne 'N') {
+    $promptInput = Read-Host -Prompt "Configure Windows Defender Exclusions? [Default: Y]"
+    if($promptInput -ne 'N') {
         Write-Output "Configuring Windows Defender...`n"
         & "$($path)\scripts\Set_Windows_Defender_Exclusions.ps1"
     }
 
 # Setup Windows Firewall
-    $input = Read-Host -Prompt "Configure Windows Firewall Rules? [Default: Y]"
-    if($input -ne 'N') {
+    $promptInput = Read-Host -Prompt "Configure Windows Firewall Rules? [Default: Y]"
+    if($promptInput -ne 'N') {
         # Define rule names
         $ruleNames = @("Tools4ever_NIM_Allow_HTTP", "Tools4ever_NIM_Allow_HTTPS")
 
         # Define ports
-        $ports = @(80, 443)
+        $ports = @($http_port, $https_port)
 
         # Loop through the ports and rule names
-
         for ($i = 0; $i -lt $ports.Count; $i++) {
             $ruleName = $ruleNames[$i]
             $port = $ports[$i]
@@ -147,11 +217,18 @@ Write-Output ""
 }
 
 # Install AD Tools
-	$input = Read-Host -Prompt "Configure AD Tools? [Default: Y]"
-    if($input -ne 'N')
+	$promptInput = Read-Host -Prompt "Configure AD Tools? [Default: Y]"
+    if($promptInput -ne 'N')
     {
         Write-Output "Configuring AD Tools...`n"
         & "$($path)\scripts\Install_AD_Tools.ps1"
     }
-    
+
+# Restart Service
+    $promptInput = Read-Host -Prompt "Restart Service? [Default: Y]"
+    if($promptInput -ne 'N') {
+        Write-Output "Restarting NIM Service..."
+        Restart-Service NIM
+    }
+
 Write-Output "`nInstallation Completed"
